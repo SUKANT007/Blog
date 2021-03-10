8cf06f88244554c02247fae820c12a2bc8f42de9
SUKANT007 <sukant.2772001@gmail.com>
2021-03-10 20:00:36 +0530

    Post section completed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,53 +1,79 @@
+import { useState, useEffect } from "react";
 import Card from "../UI/Card";
 import "./style.css";
-import profileImage from '../../assets/icons/sukant.jpg'
+import profileImage from "../../assets/icons/sukant.jpg";
+import blogPost from "../../data/blog.json";
+import { NavLink } from "react-router-dom";
+
 const Sidebar = () => {
-  return (
-    <div className="sidebarContainer">
-      <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
-		  <div className="cardHeader">
-			  <span>About Us</span>
-		  </div>
-		  <div className="profileImageContainer">
-			  <img src={profileImage} alt="Profile Image"/>
-		  </div>
-		  <div className="cardBody">
-			  <p className="personalBio">My name is Sukant Arora I am a software developer specialization in Algorithms....</p>
-		  </div>
-	  </Card>
-	  <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
-		  <div className="cardHeader">
-			  <span>Social Network</span>
-		  </div>
-	  </Card>
+	const [posts, setPosts] = useState([]);
 
-	  <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
-		  <div className="cardHeader" >
-			  <span>Recent Posts</span>
-		  </div>
-		  <div className="recentPosts">
-			  <div className="recentPost">
-				  <h3>Post Title</h3>
-				  <span>July 21,2018</span>
-			  </div>
-		  </div>
-		  <div className="recentPosts">
-			  <div className="recentPost">
-				  <h3>Post Title</h3>
-				  <span>July 21,2018</span>
-			  </div>
-		  </div>
-		  <div className="recentPosts">
-			  <div className="recentPost">
-				  <h3>Post Title</h3>
-				  <span>July 21,2018</span>
-			  </div>
-		  </div>
-	  </Card>
+	useEffect(() => {
+		const posts = blogPost.data;
+		setPosts(posts);
+	}, posts);
 
+	return (
+		<div className="sidebarContainer">
 
-    </div>
-  );
+			<Card
+				style={{
+					marginBottom: "20px",
+					padding: "20px",
+					boxSizing: "border-box",
+				}}
+			>
+				<div className="cardHeader">
+					<span>About Us</span>
+				</div>
+				<div className="profileImageContainer">
+					<img src={profileImage} alt="Profile Image" />
+				</div>
+				<div className="cardBody">
+					<p className="personalBio">
+						My name is Sukant Arora I am a software developer specialization in
+						Algorithms....
+         			</p>
+				</div>
+			</Card>
+
+			<Card
+				style={{
+					marginBottom: "20px",
+					padding: "20px",
+					boxSizing: "border-box",
+				}}
+			>
+				<div className="cardHeader">
+					<span>Social Network</span>
+				</div>
+			</Card>
+
+			<Card
+				style={{
+					marginBottom: "20px",
+					padding: "20px",
+					boxSizing: "border-box",
+				}}
+			>
+				<div className="cardHeader">
+					<span>Recent Posts</span>
+				</div>
+				<div className="recentPosts">
+					{posts.map((post) => {
+						return (
+							<NavLink to={`/post/${post.id}`}>
+								<div className="recentPost">
+									<h3>{post.blogTitle}</h3>
+									<span>{post.postedOn}</span>
+								</div>
+							</NavLink>
+						);
+					})}
+				</div>
+			</Card>
+		</div>
+	);
 };
 
 export default Sidebar;
--- src/containers/Post/index.js
+++ src/containers/Post/index.js
@@ -2,13 +2,15 @@ import BlogPost from "../../components/BlogPost";
 import Sidebar from "../../components/Sidebar";
 import Card from "../../components/UI/Card";
 import "./style.css";
-const Post = () => {
+const Post = (props) => {
+	console.log(props);
 	return (
 		<section className="container">
-			<BlogPost />
-			<Sidebar />
+			<BlogPost {...props} />
+			<Sidebar  {...props} />
 		</section>
 	);
 };
 
 export default Post;
+
