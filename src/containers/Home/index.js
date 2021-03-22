22aaa7b098b2fe06fd27a1c886657e2b61dea05a
SUKANT007 <sukant.2772001@gmail.com>
2021-03-22 19:03:11 +0530

    added adarshblogs manually

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -18,7 +18,7 @@ const ImageGallery = (props) => (
     <section style={{ width: props.largeWidth }}>
       <div className="mainImageWrapper">
         <img
-          src={require("../../blogPostImages/" + props.imagesArray[1]).default}
+          src={require("../../blogPostImages/" + props.imagesArray[6]).default}
           alt="test image"
         />
       </div>
